6fcf233097989e8743a77fb5378d5fb6253174ec
skymoon7337 <skymoon7337@gmail.com>
2025-08-17 04:20:18 +0900

    add

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,18 +15,32 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    //@Bean memberService()-> new MemoryMemberRepository()
+    //@Bean orderService()-> new MemoryMemberRepository(), new RateDiscountPolicy()
+    // 싱글톤이 깨질까 안깨질까?
+
+    // 예상 -> AppConfig.memberRepository: 3번  // 결과 -> AppConfig.memberRepository: 1번
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+    //call AppConfig.memberRepository
+
     @Bean
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
 
