c3acb8b324892254086d3947e495ffa811eb70b2
skymoon7337 <skymoon7337@gmail.com>
2025-08-31 23:56:22 +0900

    add

--- src/main/java/hello/springmvc/SpringmvcApplication.java
+++ src/main/java/hello/springmvc/SpringmvcApplication.java
@@ -3,6 +3,7 @@ package hello.springmvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringmvcApplication {
 
