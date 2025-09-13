9608efe5114441d6d994d416c02b6dce3e8b6a67
skymoon7337 <skymoon7337@gmail.com>
2025-09-13 21:09:39 +0900

    add

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -70,7 +70,7 @@ public class HomeController {
 
         Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
 
-        //세션에 회원 데이터가 없으면 home
+        //세션에 회원 데이터가 없으면 home으로
         if (loginMember == null) {
             return "home";
         }
